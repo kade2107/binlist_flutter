c15e5ffcf717cebbd397afa99b7080cbdefacd3b
Robert Mainzer <xonaman@icloud.com>
2023-01-18 21:44:04 +0100

    initial commit

---
+++ example/android/app/src/main/kotlin/com/robertmainzer/binlist/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.robertmainzer.binlist.example.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/binlist_flutter.dart
@@ -0,0 +1,2 @@
+library binlist_flutter;
+
